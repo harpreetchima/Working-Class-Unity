d565f1bdc134fed358e95390991cd046a8c45878
Harpreet <harpreet@harpreetchima.com>
2025-12-02 02:03:17 -0800

    added favicon

--- wcu-website/nuxt.config.ts
+++ wcu-website/nuxt.config.ts
@@ -3,6 +3,20 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/favicon-96x96.png', sizes: '96x96' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        { rel: 'shortcut icon', href: '/favicon.ico' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'manifest', href: '/site.webmanifest' },
+      ],
+      meta: [
+        { name: 'apple-mobile-web-app-title', content: 'WCU' },
+      ],
+    },
+  },
   css: ['~/assets/css/main.css'],
   modules: ['@nuxtjs/i18n', '@nuxt/image', '@nuxt/fonts', '@nuxt/scripts'],
   i18n: {
